63e36bfed593c55df54003f553cbed902b9a05cb
dbmage <dbmage666@gmail.com>
2019-11-02 23:48:05 +0000

    [functions] moved imports around and added my_cwd var for adding main dir to import list. Added clearqueues fucntion. Added settimers function

--- functions.py
+++ functions.py
@@ -2,10 +2,16 @@ import os
 import sys
 import json
 import requests
-import at as atq
 import RPi.GPIO as GPIO
-from datetime import datetime, timedelta
 from base64 import b64encode, b6decode
+from datetime import datetime, timedelta, date
+from workalendar.europe import UnitedKingdom
+sys.path.append(my_cwd)
+import db
+import at as atq
+
+def getPassword(password):
+    return b64decode(password).decode('utf-8')
 
 def checkTime(queue):
     timecheck = datetime.now() + timedelta(hours=1)
@@ -17,6 +23,12 @@ def checkTime(queue):
             continue
         # log message - removed job X because it is to close to the last human request
         atq.removeJob(jobid)
+
+def clearQueues():
+    for function in config['queues']:
+        for queue in config['queues'][function]:
+            atq.clearJobs(config['queues'][function][queue])
+            # Log message - Cleared jobs from queue X
 
 def setup():
     # Set Pin numbering mode
@@ -37,6 +49,32 @@ def setup():
         # Log message - initialised pin X with mode Y and state Z
         GPIO.setup(channel, GPIO.mode, initial=GPIO.defset)
 
+def setTimers():
+    clearQueues()
+    bankholidays = []
+    if config['bankholidays']:
+        for x in UnitedKingdom().holidays():
+            # Extract datetime objects from holidays
+            bankholidays.append(x[0])
+    for function in config['sets']:
+        table = config['sets'][function]
+        headers = []
+        if datetime.now() in bankholidays:
+            set = config['bankholidays']
+        dow = datetime.now().strftime("%A")
+        for x in db.describeTable(table)[2:]:
+            headers.append(x[0])
+        timers = db.selectData(table, "DAY = %s" % (dow))[0][2:]
+        for timer in range(0, len(timers)):
+            if len(timers[timer]) < 4:
+                continue
+            state = 'on'
+            if 'off' in headers[timer].lower():
+                state= 'off'
+            queue = config['queues'][function][state]
+            command = config['commands'][state]
+            atq.addJob(timer, queue, command)
+
 def resetPins():
     # Midnight failsafe - sets all pins to default setting and clears queues
     for pin in pins['active']:
@@ -47,10 +85,7 @@ def resetPins():
         defset = getattr(GPIO, pins['types'][pins['type'][pin]][pins['defaultsetting'][pin]])
         GPIO.output(pins['active'][pin], GPIO.defset)
         # Log message - set pin X to state Y
-    for function in config['queues']:
-        for queue in config['queues'][function]:
-            atq.clearJobs(config['queues'][function][queue])
-            # Log message - Cleared jobs from queue X
+    clearQueues()
 
 def on(function):
     pin = config['pins']['function'][function]
