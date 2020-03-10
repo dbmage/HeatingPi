8cad6c2712756fa0621abd3db1d588e70efcb849
dbmage <dbmage666@gmail.com>
2020-03-10 19:35:28 +0000

    [install] fixed spelling mistake

--- install.py
+++ install.py
@@ -15,7 +15,7 @@ if ! os.path.isdir(newlocation):
         print("Please create the directory and ensure the current user %s owns it" % (curuser))
         sys.exit(1)
     if pwd.getpwuid(os.stat(newlocation).st_uid)[0] != curuser:
-        print("%s exists, but is not owned by you (%s), please ensure you owne %s" % (newlocation, curuser, newlocation))
+        print("%s exists, but is not owned by you (%s), please ensure you own %s" % (newlocation, curuser, newlocation))
         sys.exit(1)
 filecopy = subprocess.Popen(['cp', '*', newlocation],
     stdin=subprocess.PIPE,
