ff38db91c7882eab64cbcf02264139f41292f282
dbmage <dbmage666@gmail.com>
2020-03-11 01:23:51 +0000

    [heating] removed global keywords

--- heating.py
+++ heating.py
@@ -6,13 +6,13 @@ import requests
 import RPi.GPIO as GPIO
 from base64 import b64encode, b64decode
 from bottle import route, run
-global my_cwd = os.path.dirname(os.path.realpath(__file__))
+my_cwd = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(my_cwd)
 import db
 import at as atq
 from functions import *
 
-global config = json.loads(open("%s/config.json" % (my_cwd)).read())
+config = json.loads(open("%s/config.json" % (my_cwd)).read())
 config['db']['db'] = "%s/%s" % ( my_cwd, config['db']['db'])
 db.connect(config['db']['db'])
 print(config)
