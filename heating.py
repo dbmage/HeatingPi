13bfbb22a710d37bec5e43d7426250a6588ff3f0
dbmage <dbmage666@gmail.com>
2020-03-11 01:36:09 +0000

    [heating] moved global vars to globals module

--- heating.py
+++ heating.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 import os
+import db
 import sys
 import json
+import globals
 import requests
+import at as atq
 import RPi.GPIO as GPIO
+from functions import *
 from base64 import b64encode, b64decode
 from bottle import route, run
-my_cwd = os.path.dirname(os.path.realpath(__file__))
-import db
-import at as atq
-from functions import *
 
-config = json.loads(open("%s/config.json" % (my_cwd)).read())
 config['db']['db'] = "%s/%s" % ( my_cwd, config['db']['db'])
 db.connect(config['db']['db'])
 print(config)
