f7ffcdc74776d30538730a219b922c780c5dedd1
dbmage <dbmage666@gmail.com>
2020-03-11 01:32:12 +0000

    [heating] removed sys.path.append

--- heating.py
+++ heating.py
@@ -7,7 +7,6 @@ import RPi.GPIO as GPIO
 from base64 import b64encode, b64decode
 from bottle import route, run
 my_cwd = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(my_cwd)
 import db
 import at as atq
 from functions import *
