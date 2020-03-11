83be5856e53a6326053d43f40d37331840dd70f2
dbmage <dbmage666@gmail.com>
2020-03-11 01:19:13 +0000

    [install.py] changed sys import to only import exit

--- install.py
+++ install.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import os
 import pwd
-import sys
 import getpass
+from sys import exit
 from distutils.dir_util import copy_tree
 
 newlocation = '/usr/local/bin/HeatingPi'
@@ -13,13 +13,14 @@ if not os.path.isdir(newlocation):
     except OSError:
         print("Creation of the directory %s failed." % (newlocation))
         print("Please create the directory and ensure the current user %s owns it" % (curuser))
-        sys.exit(1)
+        exit(1)
     if pwd.getpwuid(os.stat(newlocation).st_uid)[0] != curuser:
         print("%s exists, but is not owned by you (%s), please ensure you own %s" % (newlocation, curuser, newlocation))
-        sys.exit(1)
+        exit(1)
 try:
     copy_tree(os.path.dirname(os.path.realpath(__file__)), newlocation)
 except Exception as e:
     print("Failed to move project to %s." % (newlocation))
     print("Please check the permissions of the folder")
     print("Error: %s" % (e))
+    exit(1)
