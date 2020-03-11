b7aa46f94e8cc29b1f1c87b5f34534dd441bf905
dbmage <dbmage666@gmail.com>
2020-03-11 01:35:54 +0000

    [globals] added globals module

---
+++ globals.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+## created as per the advice in https://stackoverflow.com/questions/29803007/in-python-access-variable-defined-in-main-from-another-module
+import json
+from os import path
+my_cwd = path.dirname(path.realpath(__file__))
+config = json.loads(open("%s/config.json" % (my_cwd)).read())
