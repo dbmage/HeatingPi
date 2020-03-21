baa637a85a481eca7aceb567d59214399ec853f3
dbmage <dbmage666@gmail.com>
2020-03-21 03:36:28 +0000

    [heating] changed formatting of code

--- heating.py
+++ heating.py
@@ -11,12 +11,14 @@ from functions import *
 from lazylog import Logger
 from base64 import b64encode, b64decode
 from bottle import run, post, error, route, install, request, response, template, HTTPResponse
+
 ## Set global vars
 my_cwd = os.path.dirname(os.path.realpath(__file__))
 config = json.loads(open("%s/config.json" % (my_cwd)).read())
 config['db']['db'] = "%s/%s" % ( my_cwd, config['db']['db'])
 __builtins__.my_cwd = my_cwd
 __builtins__.config = config
+
 ## Setup logging
 config['logspecs']['level'] = getattr(log, config['logspecs']['level'], 'INFO')
 logger = Logger(config['logdir'], termSpecs=None, fileSpecs=config['logspecs'])
