8771620e2e108bc50f53dc380ee91150abe2dbd3
dbmage <dbmage666@gmail.com>
2020-03-22 03:52:22 +0000

    [install.py] removed dir owner check

--- install.py
+++ install.py
@@ -14,9 +14,6 @@ if not os.path.isdir(newlocation):
         print("Creation of the directory %s failed." % (newlocation))
         print("Please create the directory and ensure the current user %s owns it" % (curuser))
         exit(1)
-    if pwd.getpwuid(os.stat(newlocation).st_uid)[0] != curuser:
-        print("%s exists, but is not owned by you (%s), please ensure you own %s" % (newlocation, curuser, newlocation))
-        exit(1)
 try:
     copy_tree(os.path.dirname(os.path.realpath(__file__)), newlocation)
     print("Installed to %s!" % (newlocation))
