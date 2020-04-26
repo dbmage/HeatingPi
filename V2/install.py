a6b28c5d2a19fdae59810ca087990cc39eb1b75c
dbmage <dbmage666@gmail.com>
2020-04-26 19:43:17 +0100

    [install.py] added chown and chmoding

--- V2/install.py
+++ V2/install.py
@@ -3,6 +3,8 @@ import os
 import pwd
 import getpass
 from sys import exit
+from grp import getgrnam
+from pwd import getpwnam
 from distutils.dir_util import copy_tree
 
 newlocation = '/usr/local/bin/HeatingPi/'
@@ -18,6 +20,15 @@ try:
     copy_tree("%s" % (os.path.dirname(os.path.realpath(__file__))), newlocation)
     for file in [ 'install.sh', 'install.py', 'install.log', 'Package.list']:
         os.remove("%s%s" % (newlocation, file))
+    fowner = getpwnam('heatingpi').pw_uid
+    fgroup = getgrnam('www-data').gr_gid
+    for root, dirs, files in os.walk(newlocation):
+        for thing in dirs:
+            os.chown(os.path.join(root, thing), fowner, fgroup)
+            os.chmod(os.path.join(root, thing), 0o750)
+        for thing in files:
+            os.chown(os.path.join(root, thing), fowner, fgroup)
+            os.chmod(os.path.join(root, thing), 0o750)
     print("Installed to %s!" % (newlocation))
 except Exception as e:
     print("Failed to move project to %s." % (newlocation))
