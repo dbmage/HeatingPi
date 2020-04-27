b8bab4d541b41aeaad8106eb0ab6af83dd83ba55
dbmage <dbmage666@gmail.com>
2020-04-27 18:58:01 +0100

    [install.sh] made exit dynamic

--- V2/install.sh
+++ V2/install.sh
@@ -182,5 +182,6 @@ then
 fi
 if [ ! -e /usr/local/bin/HeatingPi/heating-api.wsgi ];
 then
-    sudo python3 install.py || (echo -e "\e[31mUnable to run install.py\e[33m, please run manually$RESET" && exit 1)
+    sudo python3 install.py || echo -e "\e[31mUnable to run install.py\e[33m, please run manually$RESET"
 fi
+exit $?
