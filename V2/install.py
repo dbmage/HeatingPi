3c753d8b7a4c01a9cf53db743aec7b0495fcc18d
dbmage <dbmage666@gmail.com>
2020-04-27 02:34:37 +0100

    [install.py] ask user for admin/db password

--- V2/install.py
+++ V2/install.py
@@ -6,10 +6,25 @@ import requests
 from sys import exit
 from grp import getgrnam
 from pwd import getpwnam
+from base64 import b64encode
 from distutils.dir_util import copy_tree
-
+my_cwd = os.path.dirname(os.path.realpath(__file__))
 newlocation = '/usr/local/bin/HeatingPi/'
 curuser = getpass.getuser()
+passwd = ''
+while passwd == '':
+    print("Please enter the admin password")
+    a = getpass.getpass("Password: ")
+    b = getpass.getpass("Confirm: ")
+    if a == b:
+        passwd = b64encode(getpass.encode())
+
+config = open("%s/config/config.json" % (my_cwd)).read()
+config.replace('CHANGEME', passwd)
+myfh = open("%s/config/config.json" % (my_cwd), 'w')
+myfh.write(config)
+myfh.close()
+
 if not os.path.isdir(newlocation):
     try:
         os.mkdir(newlocation)
@@ -18,7 +33,7 @@ if not os.path.isdir(newlocation):
         print("Please create the directory and ensure the current user %s owns it" % (curuser))
         exit(1)
 try:
-    copy_tree("%s" % (os.path.dirname(os.path.realpath(__file__))), newlocation)
+    copy_tree("%s" % (my_cwd), newlocation)
     for file in [ 'install.sh', 'install.py', 'install.log', 'Package.list']:
         os.remove("%s%s" % (newlocation, file))
     fowner = getpwnam('heatingpi').pw_uid
