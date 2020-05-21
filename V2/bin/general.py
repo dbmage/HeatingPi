5b93fedcc9cbdf0dcbe47c31e7df3a5417cb96e3
dbmage <dbmage666@gmail.com>
2020-05-21 04:34:32 +0100

    [bin/general] added config save function

--- V2/bin/general.py
+++ V2/bin/general.py
@@ -1,2 +1,12 @@
 def ucFirst(word):
     return "%s%s" % (word[0].upper(), word[1:])
+
+def configSave(my_cwd, config):
+    try:
+        myfh = open("%s/config/config.json" % (my_cwd), 'w')
+        myfh.write(config)
+        myfh.close()
+        return True
+    except (OSError, IOError) as e:
+        log.error("Unable to save config: %s" % (e))
+        return False
