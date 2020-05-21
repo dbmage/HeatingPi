b9aac48837ed8efeff0ef48f239404938af278eb
dbmage <dbmage666@gmail.com>
2020-05-21 21:48:24 +0100

    [sysd-service] more changes to attempt to keep wsgi alive

--- V2/service/heating-pi-init.sh
+++ V2/service/heating-pi-init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-i=0
-curl -s http://localhost:5000/test  || ((i=i+1))
-curl -s http://localhost/test || ((i=i+1))
-if [ $i -ne 0 ]; then
-    exit 1
-fi
+while :
+    if [ -f /var/run/apache2/apache2.pid ]; then
+        continue
+    fi
+    i=0
+    curl -s http://localhost:5000/test  || ((i=i+1))
+    curl -s http://localhost/test || ((i=i+1))
+    if [ $i -ne 0 ]; then
+        break
+    fi
+done
+exit 1
