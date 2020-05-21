e2a69e075d440b54bb49229e035d8fbd226223e5
dbmage <dbmage666@gmail.com>
2020-05-21 22:22:46 +0100

    [service] removed extra logic and pid checks. service reloads with apache now

--- V2/service/heating-pi-init.sh
+++ V2/service/heating-pi-init.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-apachepid=0
-while true; do
-    newpid=0
-    if [ -f /var/run/apache2/apache2.pid ]; then
-        newpid=`cat /var/run/apache2/apache2.pid`
-    fi
-    if [ "$newpid" -eq "$apachepid" ]; then
-        continue
-    fi
-    i=0
-    curl -s http://localhost:5000/test  || ((i=i+1))
-    curl -s http://localhost/test || ((i=i+1))
-    if [ $i -ne 0 ]; then
-        break
-    fi
-    apachepid=$newpid
-done
+i=0
+curl http://localhost/test || ((i=i+1))
+curl http://localhost:5000/test || ((i=i+1))
+if [ $i -eq 0 ]; then
+    exit 0
+fi
 exit 1
