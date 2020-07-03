0e358243e598a7902a5b8f95dbaef202d46b885a
dbmage <dbmage666@gmail.com>
2020-07-03 22:43:33 +0100

    [functions] fixed username key

--- V2/bin/functions.py
+++ V2/bin/functions.py
@@ -160,5 +160,5 @@ def getPinState(pin):
 
 def addUser(userdata):
     log.debug(userdata)
-    dbdata = "'%s', '%s', '%s', '%s'" % ( userdata['names'], userdata['uname'], b64encode(userdata['password'].encode()), userdata['type'] )
+    dbdata = "'%s', '%s', '%s', '%s'" % ( userdata['names'], userdata['username'], b64encode(userdata['password'].encode()), userdata['type'] )
     return db.insertData('users', dbdata)
